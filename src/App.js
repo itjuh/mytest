837fdd4df44512125ffe6f1cf5f3081139442650
itjuh <jh.2144.9679@gmail.com>
2023-11-08 12:16:32 +0900

    고양이 세팅

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={'https://pngimg.com/uploads/spongebob/spongebob_PNG32.png'} style={{borderRadius:"50%"}} className="App-logo" alt="logo" />
+        <img src={'./images/cat.png'} style={{borderRadius:"50%"}} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          귀여운게 세상을 구한다✨
         </a>
       </header>
     </div>
